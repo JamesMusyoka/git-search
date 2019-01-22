70d27670dcdadad92a1449e0e20b8feb753f48e8
James Musyoka <jamesmu475@gmail.com>
2019-01-22 17:32:44 +0300

    merge solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ProfileService } from './service/profile.service';
 
-=======
-import { ProfileService } from './service/profile.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
->>>>>>> 61d0b588eefe3bbdfeef9aced54d2229a6b504be
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import {HttpClientModule} from '@angular/common/http';
