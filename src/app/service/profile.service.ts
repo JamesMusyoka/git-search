39dc9de80e17f3ef525a3452122eb06e0eec8a0a
James Musyoka <jamesmu475@gmail.com>
2019-01-23 09:22:09 +0300

    Added a access_token

--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -11,6 +11,7 @@ export class ProfileService {
   // private repos: any[];
   private clientid = '6def820f25fb41577101';
   private clientsecret = '3e3555660560db83d02b5c0555bf21da3ab196dc';
+  private access_token = 'a6bfd7698f44e9fbf477bb74015290a8431c7483';
 
   constructor( private http: HttpClient) {
     console.log('service is now ready');
@@ -18,12 +19,12 @@ export class ProfileService {
   }
   getProfileInfo() {
      return this.http.get('https://api.github.com/users/' + this.username +
-     '?client_id=' + this.clientid + '&clientsecret=' + this.clientsecret)
+     '?access_token=' + this.access_token)
     .pipe(map( res => res));
   }
 
   getRepoInfo() {
-    return this.http.get('https://api.github.com/users/' + this.username + '/repos?clientsecret=' + this.clientsecret )
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos?access_token=' + this.access_token)
     .pipe(map(res => res));
 
   }
